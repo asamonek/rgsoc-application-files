1c42dd559f65e797873d6e6123057fe50b356f86
Aleksandra Samonek <asamonek@users.noreply.github.com>
2020-03-30 15:51:04 +0200

    Wizard Game - Python code sample
    A game made during the talkpython.fm course, showing the use of classes, etc.

---
+++ actors.py
@@ -0,0 +1,64 @@
+import random
+
+
+class Creature:
+    # level, name
+    def __init__(self, name, the_level):
+        self.name = name
+        self.level = the_level
+
+    def __repr__(self):
+        return "Creature {} of level {}.".format(self.name, self.level)
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+class Wizard(Creature):
+    # level, names
+    # Because we made wizard a creature, we do not even need an initializer method any more, we can just inherit it from the creature class
+    # def __init__(self, name, level):
+    #     super().__init__(name, level)
+
+    def attack(self, creature):
+        print('The wizard {} attacks {}!'.format(self.name, creature.name))
+
+        # my_roll = random.randint(1,12) * self.level
+        my_roll = self.get_defensive_roll()
+        # creature_roll = random.randint(1,12) * creature.level
+        creature_roll = creature.get_defensive_roll()
+
+        print('You roll {}.'.format(my_roll))
+        print('The creature rolls {}.'.format(creature_roll))
+
+        if my_roll >= creature_roll:
+            print('The wizard has triumphed over {}.'.format(creature.name))
+            return True
+        else:
+            print('The wizard has been DEFEATED by {}.'.format(creature.name))
+
+
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        # base_roll = random.randint(1,12) * self.level
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
+
+
+class Dragon(Creature):
+    def __init__(self, name, level, scaliness, breathes_fire):
+        super().__init__(name, level)
+        self.breathes_fire = breathes_fire
+        self.scaliness = scaliness
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        # fire_modifier = 1
+        # if self.breaths_fire:
+        #     fire_modifier = 5
+        # else:
+        #     fire_modifier = 1
+
+        fire_modifier = 5 if self.breathes_fire else 1
+
+        scale_modifier = self.scaliness /10
+        return base_roll * fire_modifier * scale_modifier
